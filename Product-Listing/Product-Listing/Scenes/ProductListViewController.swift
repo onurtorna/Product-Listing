3ddb1709e731dda1ac525fcc5c7c570f83d53eb1
Onur Torna <onurtorna@gmail.com>
2018-10-14 22:33:58 +0300

    Use product cell in product list

--- Product-Listing/Product-Listing/Scenes/ProductListViewController.swift
+++ Product-Listing/Product-Listing/Scenes/ProductListViewController.swift
@@ -10,9 +10,65 @@ import UIKit
 
 class ProductListViewController: UIViewController {
 
+    private enum Constant {
+        static let collectionViewCellPadding: CGFloat = 20.0
+    }
+
+    @IBOutlet private weak var collectionView: UICollectionView!
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        collectionView.collectionViewLayout.invalidateLayout()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        collectionView.dataSource = self
+
+        collectionView.register(ProductCollectionViewCell.defaultNib,
+                                forCellWithReuseIdentifier: ProductCollectionViewCell.defaultNibName)
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension ProductListViewController: UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.size.width / 2 - (Constant.collectionViewCellPadding * 2),
+                      height: ProductCollectionViewCell.cellSize)
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: Constant.collectionViewCellPadding,
+                            left: Constant.collectionViewCellPadding,
+                            bottom: Constant.collectionViewCellPadding,
+                            right: Constant.collectionViewCellPadding)
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension ProductListViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: ProductCollectionViewCell.defaultNibName,
+            for: indexPath) as? ProductCollectionViewCell {
+            // TODO:
+            return cell
+        }
+
+        return UICollectionViewCell()
     }
 }
 
