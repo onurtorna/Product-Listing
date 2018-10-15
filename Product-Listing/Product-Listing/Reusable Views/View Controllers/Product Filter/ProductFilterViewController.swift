5a15a32ab1966e91dac029e3068d004d0048d51b
Onur Torna <onurtorna@gmail.com>
2018-10-16 00:19:53 +0300

    Add filter view controller and view model

---
+++ Product-Listing/Product-Listing/Reusable Views/View Controllers/Product Filter/ProductFilterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProductFilterViewController.swift
+//  Product-Listing
+//
+//  Created by Onur Torna on 16.10.2018.
+//  Copyright © 2018 Onur Torna. All rights reserved.
+//
+
+import UIKit
+
+final class ProductFilterViewController: UIViewController {
+
+    var viewModel: ProductFilterViewModel!
+}
+
+// MARK: - NibLoadable
+extension ProductFilterViewController: NibLoadable { }
---
+++ Product-Listing/Product-Listing/Reusable Views/View Controllers/Product Filter/ProductFilterViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  ProductFilterViewModel.swift
+//  Product-Listing
+//
+//  Created by Onur Torna on 16.10.2018.
+//  Copyright © 2018 Onur Torna. All rights reserved.
+//
+
+import Foundation
+
+final class ProductFilterState {
+
+    enum Change {
+
+    }
+}
+
+final class ProductFilterViewModel {
+
+    private let state = ProductFilterState()
+    // TODO:
+}
