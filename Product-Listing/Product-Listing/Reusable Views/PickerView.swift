87aec231a44d961ea67d32c98c1335bf2f9bb646
Onur Torna <onurtorna@gmail.com>
2018-10-16 01:27:41 +0300

    Add picker view for option selection

---
+++ Product-Listing/Product-Listing/Reusable Views/PickerView.swift
@@ -0,0 +1,115 @@
+//
+//  PickerView.swift
+//  Product-Listing
+//
+//  Created by Onur Torna on 16.10.2018.
+//  Copyright © 2018 Onur Torna. All rights reserved.
+//
+
+import UIKit
+
+protocol PickerViewDelegate: class {
+
+    func pickerViewDidSelectItem(_ view: PickerView,
+                                 selectedItem: ConfigurableAttributeOption)
+}
+
+final class PickerView: PLView {
+
+    @IBOutlet private weak var textField: UITextField!
+
+    private let pickerView = UIPickerView()
+
+    var configuration: ConfigurableAttribute! {
+        didSet {
+            textField.placeholder = configuration.code == .color ? "COLOR" : "SIZE"
+        }
+    }
+
+    weak var delegate: PickerViewDelegate?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        layer.cornerRadius = 8.0
+        layer.borderColor = UIColor.black.cgColor
+        layer.borderWidth = 1.0
+
+        preparePickerView()
+    }
+}
+
+extension PickerView {
+
+    func preparePickerView() {
+
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        // TODO: Localize
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace,
+                                          target: nil,
+                                          action: nil)
+        let doneButton = UIBarButtonItem(title: "Ok",
+                                         style: UIBarButtonItem.Style.done,
+                                         target: self,
+                                         action: #selector(selectItem))
+        toolbar.setItems([spaceButton, doneButton], animated: false)
+
+        textField.inputAccessoryView = toolbar
+        
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        pickerView.backgroundColor = .white
+        textField.inputView = pickerView
+    }
+}
+
+// MARK: - Actions
+private extension PickerView {
+
+    @objc func selectItem() {
+
+        let row = pickerView.selectedRow(inComponent: 0)
+        if let option = configuration.options?[row] {
+            textField.text = option.label
+            delegate?.pickerViewDidSelectItem(self,
+                                              selectedItem: option)
+        }
+        endEditing(true)
+    }
+}
+
+// MARK: - UIPickerViewDelegate
+extension PickerView: UIPickerViewDelegate {
+
+    func pickerView(_ pickerView: UIPickerView,
+                    viewForRow row: Int,
+                    forComponent component: Int,
+                    reusing view: UIView?) -> UIView {
+
+        var pickerLabel = view as? UILabel
+
+        if pickerLabel == nil {
+
+            pickerLabel = UILabel()
+            pickerLabel?.textColor = .black
+            pickerLabel?.textAlignment = .center
+        }
+
+        pickerLabel?.text = configuration.options?[row].label
+        return pickerLabel ?? UILabel()
+    }
+}
+
+// MARK: - UIPickerViewDataSource
+extension PickerView: UIPickerViewDataSource {
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView,
+                    numberOfRowsInComponent component: Int) -> Int {
+        return configuration.options?.count ?? 0
+    }
+}
